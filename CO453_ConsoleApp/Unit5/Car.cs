5d807a8483b7504296a0e960390ec688cb2bf439
DerekPeacock <derek.peacock@yahoo.com>
2019-02-11 20:04:27 +0000

    Added example classes

---
+++ CO453_ConsoleApp/Unit5/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CO453_ConsoleApp.Unit5
+{
+    public class Car
+    {
+        // attribue
+        private string make = "Honda";
+
+        // property
+        public string Model { get; set; }
+
+        public Car()
+        {
+            Model = "Jazz";
+        }
+
+        public Car(string make, string model)
+        {
+            this.make = make;
+            this.Model = model;
+        }
+
+        // Method
+        public void Drive()
+        {
+
+        }
+
+    }
+}
